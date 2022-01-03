02af93f2937046de3940c54845e636592b5cbde9
JustusMzB <ju@mzb.de>
2022-01-03 17:19:44 +0100

    Postum correction of non-staged changes

--- testproject/testapp/urls.py
+++ testproject/testapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from testapp.views import hello_world
+from testapp.views import hello_world, hello_name
 
 urlpatterns = [
     path('helloworld/', hello_world, name = 'helloworld'),
+    path('helloname/<str:name>/', hello_name, name = 'helloname'),
 ]
--- testproject/testapp/views.py
+++ testproject/testapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django import template
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+def hello_world(request):
+    return HttpResponse('Hello World')
+
+def hello_name(request, name):
+   template = loader.get_template('testapp/helloname.html')
+   name = list(name)
+   context = {'name':name}
+   return HttpResponse(template.render(context, request))
