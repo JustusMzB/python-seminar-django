1bf50e19fcf678dbfb3726247d43441af3e6465b
JustusMzB <ju@mzb.de>
2022-01-03 17:59:44 +0100

    registered models for admin and added superuser

--- testproject/testapp/admin.py
+++ testproject/testapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import HttpCall, Parameter
+
+admin.site.register([HttpCall, Parameter])
+#Admin account is:
+#username: admin
+#mail: admin@admin.admin
+#pw: admin
