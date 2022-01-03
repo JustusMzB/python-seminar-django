9facb9ac6a19c5e6f5c5b907078d26e69f386080
JustusMzB <ju@mzb.de>
2022-01-03 17:43:48 +0100

    added models and made migrations

--- testproject/testapp/models.py
+++ testproject/testapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class HttpCall(models.Model):
+   url = models.URLField()
+   
+class Parameter(models.Model):
+   name = models.CharField(max_length=300)
+   value = models.CharField(max_length=300)
+   call = models.ForeignKey(HttpCall, on_delete=models.CASCADE)
